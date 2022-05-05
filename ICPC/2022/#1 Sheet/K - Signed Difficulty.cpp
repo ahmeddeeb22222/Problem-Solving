bd753affd36aedc81851b5211ad038668490d0ec
ahmeddeeb22222 <53723591+ahmeddeeb22222@users.noreply.github.com>
2022-05-05 07:28:02 +0200

    Update K - Signed Difficulty.cpp

--- ICPC/2022/#1 Sheet/K - Signed Difficulty.cpp
+++ ICPC/2022/#1 Sheet/K - Signed Difficulty.cpp
@@ -17,10 +17,11 @@ using namespace std;
 
 int main() { IOS
     dd n{}; cin >> n;
-    int int_n = n*10; // to get y we need to mod int value so we convert it into int thru multiplying by 10
+    int int_n = n*10;
     int y = int_n % 10; // mod 10 get us the last digit which is y
+    int x = n; // casting from double to int to get x
 
-    if(y <= 2) cout << n << "-";
-    else if(y <= 6) cout << n;
-    else cout << n << "+";
+    if(y <= 2) cout << x << "-";
+    else if(y <= 6) cout << x;
+    else cout << x << "+";
 }
