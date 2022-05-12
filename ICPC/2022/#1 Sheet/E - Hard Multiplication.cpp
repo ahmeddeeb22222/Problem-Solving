7662d869a6a10cc5a41fa2512ec0e138a2cd7150
ahmeddeeb22222 <53723591+ahmeddeeb22222@users.noreply.github.com>
2022-05-12 08:14:56 +0200

    Update E - Hard Multiplication.cpp

--- ICPC/2022/#1 Sheet/E - Hard Multiplication.cpp
+++ ICPC/2022/#1 Sheet/E - Hard Multiplication.cpp
@@ -1,4 +1,4 @@
-                                                    // problem link: Gym - 350741A
+                                                    // problem: Gym - 350741A
 #include <bits/stdc++.h>
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define ll long long
