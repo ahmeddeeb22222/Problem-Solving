1cdbafeb2d095e4088c661a678d4436b4501092b
ahmeddeeb22222 <53723591+ahmeddeeb22222@users.noreply.github.com>
2022-05-15 11:59:02 +0200

    Delete ICPC/2022 directory

--- ICPC/2022/#1 Sheet Assiut/A. Say Hello With C++.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define ll long long
-using namespace std;
-
-int main() { IOS
-    string s; cin >> s;
-    cout << "Hello, " << s;
-}
